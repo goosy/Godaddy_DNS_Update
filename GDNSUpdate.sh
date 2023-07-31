e3469a6d4466c4dc949bdfc8ecf8328067faae60
goosy <goosy.jo@gmail.com>
2023-08-01 03:11:30 +0800

    determine the IP version based on the IP address

--- GDNSUpdate.sh
+++ GDNSUpdate.sh
@@ -14,36 +14,47 @@ records=(
 
 auth_header="Authorization: sso-key $GD_Key:$GD_Secret"
 # echo $auth_header
+IPV4_reg='(([0-9]{1,3}\.){3}[0-9]{1,3})'
+IPV6_reg='(([0-9a-f]{1,4}:){7}[0-9a-f]{1,4})'
 
 record_update() {
-    myIP="$1"
-    url="$2"
+    myIP="$3"
+    url=''
+    if [[ "$myIP" =~ ^$IPV4_reg$ ]]; then
+        url="https://api.godaddy.com/v1/domains/$1/records/A/$2"
+        reg="\b$IPV4_reg\b"
+    fi
+    if [[ "$myIP" =~ ^$IPV6_reg$ ]]; then
+        url="https://api.godaddy.com/v1/domains/$1/records/AAAA/$2"
+        reg="\b$IPV6_reg\b"
+    fi
+    if [ "$url" = '' ]; then
+        echo "IP is warning"
+        return 1 # return value 1 on failure
+    fi
     dns_data=$(curl -s -X GET -H "$auth_header" "$url")
     # echo $dns_data
-    GDIP=$(
-        echo $dns_data | 
-        grep -oE '\b(([0-9]{1,3}\.){3}[0-9]{1,3})|(([0-9a-f]{1,4}:){7}[0-9a-f]{1,4})\b'
-    )
+    GDIP=$(echo $dns_data | grep -oE $reg)
     # echo "$myIP" "$GDIP" "$url"
-    if [ "$GDIP" != "$myIP" -a "$myIP" != "" ]; then
+    if [ "$GDIP" != "$myIP" ] && [ "$myIP" != "" ]; then
         # echo "Ips are not equal"
-        request='[{"data":"'$myIP'","ttl":600}]'
-        # echo "request:" $request
-        result=$(curl -i -s -X PUT \
+        req_data='[{"data":"'$myIP'","ttl":600}]'
+        # echo "req_data:" "$req_data"
+        result=$(curl -i -s -X PUT "$url" \
             -H "$auth_header" \
             -H "Content-Type: application/json" \
-            -d $request \
-            "$url")
-        # echo "result:" $result
+            -d $req_data
+        )
+        # echo "result:" "$result"
     fi
 }
 
 DNS_update() {
     prefix_url="https://api.godaddy.com/v1/domains/$1/records"
     # update A record
-    record_update $(curl -s "https://api.ipify.org") "$prefix_url/A/$2"
+    record_update $1 $2 $(curl -s "https://api.ipify.org")
     # update AAAA record
-    record_update $(curl -s "https://api6.ipify.org") "$prefix_url/AAAA/$2"
+    record_update $1 $2 $(curl -s "https://api6.ipify.org")
 }
 
 for record in "${records[@]}"; do
